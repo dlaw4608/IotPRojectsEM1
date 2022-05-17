b802980861f68751e9c560f0385c97fcf3506d65
dlaw4608 <91201621+dlaw4608@users.noreply.github.com>
2022-05-17 11:47:30 +0100

    Update Final Fully Working Script.py

--- Final Fully Working Script.py
+++ Final Fully Working Script.py
@@ -1,21 +1,21 @@
-/* Smart Dispenser
- by Daniel Lawton
- 17th/05/2022
+# Smart Dispenser
+ #by Daniel Lawton
+ #17th/05/2022
  
- All refrences found in file in repository
+# All refrences found in file in repository
  
 */
-//import libraries
+#import libraries
 import BlynkLib                                           
-import RPi.GPIO as GPIO                                   
+import RPi.GPIO as GPIO  #choose module                                 
 import time
 import signal
 import atexit
 
-//User authentication connect to Blynk account
+#User authentication connect to Blynk account
 BLYNK_AUTH = "17McbeumaK2cn8OB5oacJXpYqMUwSt_J"
 
-//shutdown handler
+#shutdown handler
 atexit.register(GPIO.cleanup)            
 
 //defines sensor and servo pins on Raspberry Pi
@@ -34,10 +34,10 @@ p = GPIO.PWM(servopin, 50)
 p.start(0)
 time.sleep(2)
 
-//Intialiise Blynlk
+#Intialiise Blynlk
 blynk = BlynkLib.Blynk(BLYNK_AUTH)       
 
-//A basic method which prints and turns the servo motor when motion is detected
+#A basic method which prints and turns the servo motor when motion is detected
 def MOTION():
     p.ChangeDutyCycle(10)
     time.sleep(1.2)
@@ -48,7 +48,7 @@ def MOTION():
     print( "Motion Detected!" )
     blynk.virtual_write(2, GPIO.input(sensorpin))
 
-//A try if statement only true when motion is detected.
+#A try if statement only true when motion is detected not utitlised
 def move_sensor():
 
        try:
@@ -59,17 +59,17 @@ def move_sensor():
         
       
 
-// register handler for virtual pin V4 write event
+#register handler for virtual pin V4 write event
 @blynk.on("V4")
 def v4_write_handler(value):
     buttonValue=value[0]
     print(f'Current button value: {buttonValue}')
     
-    //Shares Data with V2 virtual pin (LED)
+    #Shares Data with V2 virtual pin (LED)
     
     blynk.virtual_write(2, buttonValue)
     
-    //Configures Servo movements
+    #Configures Servo movements
     p.ChangeDutyCycles(5)
     time.sleep(0.5)
     p.ChangeDutyCycle(0)
@@ -83,7 +83,7 @@ def v4_write_handler(value):
     p.ChangeDutyCycle(0)
     time.sleep(0.2)
 
-//A try if statement only true when motion is detected
+#A try if statement only true when motion is detected
 try:
     while True:
         if GPIO.event_detected(sensorpin):
